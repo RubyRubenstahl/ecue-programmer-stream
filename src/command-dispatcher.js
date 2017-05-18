0d243ca0b37c753957df36de6dae9bec46a5d9df
Mike Rubenstahl <Mike Rubenstahl>
2017-05-18 19:57:11 -0400

    general improvements

--- src/command-dispatcher.js
+++ src/command-dispatcher.js
@@ -3,32 +3,38 @@
  */
 const Rx = require('rxjs');
 
-
-function commandDispatcher(socket, commandStream=Rx.Observable.never(),options={address:'127.0.0.1', port:9001}){
-
-    const {address, port}= options;
-
-
+/**
+ * @function commandDispatcher
+ * @param socket {Object} socket - An initialized dgram socket.
+ * @param options {Object} [options] - port and/or address. Defaults: {address: 'localhost', 9001}
+ * @param commandStream -
+ * @returns {{send: send, address: string, port: number}}
+ */
+function commandDispatcher(socket, options, commandStream=Rx.Observable.never()){
+    const address = options.address || 'localhost';
+    const port = options.port || 9001;
     commandStream.subscribe(command=>send(command));
 
     function send(command){
-
-        sendPackedCommand(packCommand(command));
-    }
-
-    function sendPackedCommand(packedCommand){
-        console.log(`Sending ${packedCommand} to ${address}:${port}`)
+        const packedCommand = packCommand(command);
+            console.log(`Sending ${packedCommand} to ${address}:${port}`)
         socket.send(packedCommand, port, address, (err) => {
             if(err) console.log(err);
-            console.log('sent');
         });
     }
 
     return {send, address, port}
 }
 
+/* Takes in a command object of the shape {command: "MY_COMMAND", params[1, "SOME STRING"]...}}
+   and transforms it into an a tab-separated string starting with the command followed by each
+   of the parameters in order. Each command is ended with a newline.
+
+ */
 function packCommand({command, params=[]}){
     return ([command].concat(params)).join('\t')+'\n';
 }
+
+
 
 module.exports = commandDispatcher;
--- test/test.js
+++ test/test.js
@@ -3,19 +3,19 @@
  */
 
 const dgram = require('dgram');
-const socket = dgram.createSocket('udp4');
+const socket = dgram.createSocket('udp4', {address: '192.168.123.19'});
 socket.bind(4000);
 
 const ecuePogrammerStream = require('../src/ecue-programmer-stream')
 const stateStream = ecuePogrammerStream.StateStream(socket);
-const commandDispatcher = ecuePogrammerStream.CommandDispatcher(socket);
+const commandDispatcher = ecuePogrammerStream.CommandDispatcher(socket, {address:'192.168.123.11'});
 
 stateStream.all.subscribe(
     packet=>console.log(packet)
-)
+);
 
 setInterval(()=>{
-    console.log(commandDispatcher)
+    console.log(commandDispatcher);
     commandDispatcher.send({command: 'CUELIST_PLAY', params:[1,3]});
-},2000)
+},2000);
 
